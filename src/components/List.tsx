7835000106013a7af5c05531f57b1604325c4b4e
Anthony Lu <anthony.lu1115@gmail.com>
2022-02-09 21:19:21 +1100

    setup initial typescript enum

---
+++ src/components/List.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+interface IProps {
+  people: { name: string; age: number; url: string; note?: string }[];
+}
+const List = (props: IProps) => {
+  return <div></div>;
+};
+
+export default List;
